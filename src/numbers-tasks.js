e050c50c82d289c6ec9b17a32c4b1696704061a2
Vladislav-bot203 <70837097+Vladislav-bot203@users.noreply.github.com>
2024-11-10 12:50:25 +0300

    feat: implement getCircleCircumference function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -11,19 +11,8 @@ function getRectangleArea(width, height) {
   return width * height;
 }
 
-/**
- * Returns a circumference of circle given by radius.
- *
- * @param {number} radius
- * @return {number}
- *
- * @example:
- *   5    => 31.41592653589793
- *   3.14 => 19.729201864543903
- *   0    => 0
- */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return Math.PI * radius * 2;
 }
 
 /**
