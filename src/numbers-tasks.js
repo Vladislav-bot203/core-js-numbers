0eb4b60d6c51b899208cf17457b8f3888bf73c2e
Vladislav-bot203 <70837097+Vladislav-bot203@users.noreply.github.com>
2024-11-10 17:39:09 +0300

    feat: implemented toNumber function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -40,24 +40,15 @@ function isPrime(n) {
   }
   return isPr;
 }
-
-/**
- * Tries to convert value to number and returns it if conversion was successful;
- * otherwise returns default value passed as a second argument.
- *
- * @param {any} value
- * @param {any} def
- * @return {number}
- *
- * @example
- *   toNumber(null, 0) => 0
- *   toNumber('test', 0) => 0
- *   toNumber('1', 0) => 1
- *   toNumber(42, 0) => 42
- *   toNumber(new Number(42), 0) => 42
- */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const num = +value;
+  let result = null;
+  if (typeof num === 'number' && !Number.isNaN(num)) {
+    result = num;
+  } else {
+    result = def;
+  }
+  return result;
 }
 
 /**
