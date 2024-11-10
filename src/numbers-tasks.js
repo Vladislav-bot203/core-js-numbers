429581cc678b5c4312b2d9c97c36353efe9c4a79
Vladislav-bot203 <70837097+Vladislav-bot203@users.noreply.github.com>
2024-11-10 12:43:08 +0300

    feat: implement getRectangleArea function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -7,19 +7,8 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-/**
- * Returns an area of a rectangle given by width and height.
- *
- * @param {number} width
- * @param {number} height
- * @return {number}
- *
- * @example:
- *   5, 10 => 50
- *   5, 5  => 25
- */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
 
 /**
