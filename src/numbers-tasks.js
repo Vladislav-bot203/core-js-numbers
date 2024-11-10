9dbefa9d653ca1dab356f25018b619f577703bf7
Vladislav-bot203 <70837097+Vladislav-bot203@users.noreply.github.com>
2024-11-10 17:45:10 +0300

    feat: getCube function implementation

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -50,20 +50,8 @@ function toNumber(value, def) {
   }
   return result;
 }
-
-/**
- * Returns the cube of the given number.
- *
- * @param {number} num
- * @return {number}
- *
- * @example:
- *   3  => 27
- *   -2 => -8
- *   0  => 0
- */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
