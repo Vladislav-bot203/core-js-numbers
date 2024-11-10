f604697361df388caeef6be71bbae1d4cbba6294
Vladislav-bot203 <70837097+Vladislav-bot203@users.noreply.github.com>
2024-11-10 15:02:56 +0300

    feat: implemented getAngleBetweenVectors function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -18,25 +18,11 @@ function getLinearEquationRoot(a, b) {
   return -b / a;
 }
 
-/**
- * Returns an angle (in radians) between two vectors given by xi and yi,
- * coordinates in Cartesian plane.
- * See details https://en.wikipedia.org/wiki/Euclidean_vector#Representations
- *
- * @param {number} x1
- * @param {number} y1
- * @param {number} x2
- * @param {number} y2
- * @return {number}
- *
- * @example:
- *   (1,0) (0,1)     => π/2
- *   (0,1) (0,-1)    => π
- *   (0,-1) (1,0)    => π/2
- *   (0,1) (0,1)     => 0
- */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  return Math.acos(
+    (x1 * x2 + y1 * y2) /
+      ((x1 ** 2 + y1 ** 2) ** (1 / 2) * (x2 ** 2 + y2 ** 2) ** (1 / 2))
+  );
 }
 
 /**
