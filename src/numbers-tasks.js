26b8d53a299e55254c01ca8167221eb9c03af980
Vladislav-bot203 <70837097+Vladislav-bot203@users.noreply.github.com>
2024-11-10 16:57:43 +0300

    feat: implemented isPrime function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -32,26 +32,13 @@ function roundToPowerOfTen(num, pow) {
   const float = num * 10 ** -pow;
   return Math.round(float) * 10 ** pow;
 }
-
-/**
- * Returns true is the number is prime; otherwise false.
- * See: https://en.wikipedia.org/wiki/Primality_test
- *
- * @param {number} n
- * @return {bool}
- *
- * @example:
- *   4 => false
- *   5 => true
- *   6 => false
- *   7 => true
- *   11 => true
- *   12 => false
- *   16 => false
- *   17 => true
- */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let isPr = null;
+  for (let i = 2; i <= n; i += 1) {
+    if (i === 2) isPr = true;
+    if (n % i === 0 && n !== i) isPr = false;
+  }
+  return isPr;
 }
 
 /**
