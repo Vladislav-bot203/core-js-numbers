79472969a6019a90d0ea7f94f8108b4ce2818c3a
Vladislav-bot203 <70837097+Vladislav-bot203@users.noreply.github.com>
2024-11-10 13:51:32 +0300

    feat: implemented getAverage function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -15,20 +15,8 @@ function getCircleCircumference(radius) {
   return Math.PI * radius * 2;
 }
 
-/**
- * Returns an average of two given numbers.
- *
- * @param {number} value1
- * @param {number} value2
- * @return {number}
- *
- * @example:
- *   5, 5  => 5
- *  10, 0  => 5
- *  -3, 3  => 0
- */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  return value1 / 2 + value2 / 2;
 }
 
 /**
@@ -46,8 +34,8 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return pow(pow(x1 + x2, 2) + pow(y1 + y2, 2), 1 / 2);
 }
 
 /**
