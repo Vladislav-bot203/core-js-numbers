a30cf17fc243041d67da18e0c6cbce3b98a5cdea
Vladislav Naftolin <vnaftolin228@gmail.com>
2024-11-11 12:55:08 +0300

    feat: implemented numberToStringInBase function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -87,20 +87,8 @@ function isPowerOfTwo(num) {
 function getSine(num) {
   return Math.sin(num);
 }
-
-/**
- * Returns a string representation of a number in a specified base (radix).
- *
- * @param {number} number
- * @param {number} base
- * @return {string}
- *
- * @example:
- * 255, 16 => 'ff'
- * 2, 2    => '10'
- */
-function numberToStringInBase(/* number, base */) {
-  throw new Error('Not implemented');
+function numberToStringInBase(number, base) {
+  return number.toString(base);
 }
 function toExponential(number, fractionDigits) {
   return number.toExponential(fractionDigits);
