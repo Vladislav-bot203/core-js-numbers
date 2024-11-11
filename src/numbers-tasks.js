8ef4c9ae37ce64d0a2d600f0bb995a916f995870
Vladislav Naftolin <vnaftolin228@gmail.com>
2024-11-11 12:22:11 +0300

    feat: implemented roundToLargestInteger function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -166,19 +166,8 @@ function isSafeInteger(number) {
 function roundToSmallestInteger(number) {
   return Math.floor(number);
 }
-
-/**
- * Returns the largest integer greater than or equal to a given number.
- *
- * @param {number} number
- * @return {number}
- *
- * @example:
- * 5.1  => 6
- * -5.9 => -5
- */
-function roundToLargestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToLargestInteger(number) {
+  return Math.ceil(number);
 }
 
 /**
