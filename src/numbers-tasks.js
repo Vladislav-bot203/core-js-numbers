1dc32c69a83d9d35fc9027e84648dc6403862d98
Vladislav Naftolin <vnaftolin228@gmail.com>
2024-11-11 12:15:52 +0300

    feat: implemented getIntegerOnString function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -157,23 +157,8 @@ function isInteger(number) {
 function getFloatOnString(str) {
   return Number.parseFloat(str);
 }
-
-/**
- * Returns an integer of the specified base or, if the number cannot be parsed
- * from the argument, returns NaN.
- *
- * @param {string} str
- * @param {number} base
- * @return {number | NaN}
- *
- * @example:
- * '4.567abcdefgh', 10  => 4
- * 'abcdefgh', 10       => NaN
- * '1.234', 2           => 1
- * '10', 8              => 8
- */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  return Number.parseInt(str, base);
 }
 
 /**
